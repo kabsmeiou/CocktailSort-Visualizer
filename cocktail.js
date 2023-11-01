f2076b070c226454322f6816bb2270ed5a3bb882
hopskiprabbit <94173937+kabsmeiou@users.noreply.github.com>
2023-11-01 15:38:52 +0800

    section 2 changes
    key features added for playing the animation and generating the array.

--- cocktail.js
+++ cocktail.js
@@ -1,59 +1,73 @@
-const size = 50; //array size
+const size = 30; //array size
 const array = [];
-
+let flag = false; //a flag to check if the animation is still ongoing
 //creating the array using random number generator
 function randomize() {
+    if (flag) return;   //if its still animating, then dont generate another array
     for (let elem = 0; elem < size; elem++) {
-        array[elem] = Math.random();
+        array[elem] = Math.random() * 15 + 1;
     }
     display();
 }
 
 function play() {
-    const copy =[...array];
-    const swaps = cocktailSort(copy);
-    animate(swaps);
+    if (flag) return; 
+    const copy = [...array];
+    const moves = cocktailSort(copy);
+    animate(moves);
 }
 
-
-//animation for the swaps
-function animate(swaps) {
-    if (swaps.length == 0) {
+//animation for the moves
+function animate(moves) {
+    flag = true;
+    if (moves.length == 0) {
+        const element = document.querySelectorAll('.bar');
+        element.forEach(bar => {
+            bar.classList.add('glow-effect');
+        });
+        flag = false;
         return;
     }
-    const [i, j] = swaps.shift();
-    [array[i], array[j]] = [array[j], array[i]];
-    display();
+    const move = moves.shift();
+    const [i, j] = move.indices;
+    if (move.type == 'swap') {
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    display(move);
     setTimeout(()=> {
-        animate(swaps);
-    }, 20);
+        animate(moves);
+    }, 8);
 }
 
 //call the array generator for the first iteration
 randomize();
 
 //displaying the array 
-function display() {
+function display(move) {
     container.innerHTML = "";
     for (let elem = 0; elem < size; elem++) {
         const bar = document.createElement("div");
-        bar.style.height = array[elem] * 100 + "%";
+        bar.style.height = array[elem] * 10 + "%";
         bar.classList.add("bar");
+        if (move && move.indices.includes(elem)) {
+            bar.style.background = move.type == 'swap' ? "red" : "blue";
+        }
         container.appendChild(bar);
     }
 }
 
 //cocktail sort algorithm
 function cocktailSort(array) {
-    const swaps = [];
+    const moves = [];
     var forwardSwap, backwardSwap;
     do {
         forwardSwap = false, backwardSwap = false;
         //forward pass
         for (let index = 0; index < size - 1; index++) {
+            moves.push({indices: [index, index + 1], type: "compare"});
             if (array[index] > array[index + 1]) {
                 forwardSwap = true;
-                swaps.push([index, index + 1]);
+                moves.push({indices: [index, index + 1], type: "swap"});
                 var temp = array[index];
                 array[index] = array[index + 1];
                 array[index + 1] = temp;
@@ -61,16 +75,36 @@ function cocktailSort(array) {
         }
         //backward pass
         for (let index = size - 2; index >= 0; index--) {
+            moves.push({indices: [index, index + 1], type: "compare"});
             if (array[index + 1] < array[index]) {
                 backwardSwap = true;
-                swaps.push([index, index + 1]);
+                moves.push({indices: [index, index + 1], type: "swap"});
                 var temp = array[index];
                 array[index] = array[index + 1];
                 array[index + 1] = temp;
             }
         }
     } while (forwardSwap || backwardSwap);
-    return swaps;
+    return moves;
 }
 
+
+//for the shaking animation after clicking play
+const playButton = document.getElementById('playButton');
+
+playButton.addEventListener('click', () => {
+    playButton.classList.add('shake');
+        setTimeout(() => {
+        playButton.classList.remove('shake');
+    }, 300);
+});
+
+const randomizerButton = document.getElementById('random');
+
+randomizerButton.addEventListener('click', () => {
+    randomizerButton.classList.add('shake');
+        setTimeout(() => {
+            randomizerButton.classList.remove('shake');
+    }, 300);
+});
 
